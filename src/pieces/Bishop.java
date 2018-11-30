5de2a9fac066d0e2252a8bae56ddcf603100da3a
MasterEman <emanuel.kulich@gmail.com>
2018-11-30 12:45:20 +0100

    update

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -4,6 +4,7 @@ import board.ChessBoard;
 import board.Square;
 import javafx.scene.paint.Color;
 
+@SuppressWarnings("unused")
 public class Bishop extends Piece {
 
 	public Bishop(Color c) {
@@ -11,13 +12,13 @@ public class Bishop extends Piece {
 	}
 
 	public void showMove(int x, int y) {
-
+/*
 		// Up Left
 		for (int i = 1; i < 8; i++) {
 			if (x - i < 0) {
 				break;
 			}
-			Square s1 = ChessBoard.getSquare(x - i, y);
+			Square s1 = ChessBoard.getSquare(x + i, y - i);
 			if (!s1.hasPiece()) {
 				s1.mark(getColor());
 			} else {
@@ -31,7 +32,7 @@ public class Bishop extends Piece {
 			if (x + i > 7) {
 				break;
 			}
-			Square s1 = ChessBoard.getSquare(x + i, y);
+			Square s1 = ChessBoard.getSquare(x + i, y - i);
 			if (!s1.hasPiece()) {
 				s1.mark(getColor());
 			} else {
@@ -45,7 +46,7 @@ public class Bishop extends Piece {
 			if (y - i < 0) {
 				break;
 			}
-			Square s1 = ChessBoard.getSquare(x, y - i);
+			Square s1 = ChessBoard.getSquare(x + i, y - i);
 			if (!s1.hasPiece()) {
 				s1.mark(getColor());
 			} else {
@@ -59,15 +60,16 @@ public class Bishop extends Piece {
 			if (y + i > 7) {
 				break;
 			}
-			Square s1 = ChessBoard.getSquare(x, y + i);
+			Square s1 = ChessBoard.getSquare(x + i, y - i);
 			if (!s1.hasPiece()) {
 				s1.mark(getColor());
 			} else {
 				s1.mark(getColor());
 				break;
 			}
-		}
+		} */
 	}
+
 
 	public void hideMove() {
 
--- src/pieces/King.java
+++ src/pieces/King.java
@@ -1,23 +1,21 @@
 package pieces;
 
+import board.ChessBoard;
+import board.Square;
 import javafx.scene.paint.Color;
 
+@SuppressWarnings("unused")
 public class King extends Piece{
 	
 	public King(Color c){
 		super(c);
 	}
 
-	@Override
 	public void showMove(int x, int y) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void hideMove() {
-		// TODO Auto-generated method stub
 		
 	}
-
 }
