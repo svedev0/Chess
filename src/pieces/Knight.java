98b1de276b57391f132c138a4053dd65a2eab82e
MasterEman <emanuel.kulich@gmail.com>
2018-12-05 13:41:05 +0100

    update

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -1,5 +1,7 @@
 package pieces;
 
+import board.ChessBoard;
+import board.Square;
 import javafx.scene.paint.Color;
 
 public class Knight extends Piece {
@@ -9,6 +11,32 @@ public class Knight extends Piece {
 	}
 
 	public void showMove(int x, int y) {
+
+		// Up Right
+		if (y - 2 >= 0 && x + 1 < 8) {
+			Square s1 = ChessBoard.getSquare(x + 1, y - 2);
+			s1.moveMark();
+		}
+
+		// Up Left
+		if (y - 2 >= 0 && x - 1 >= 0) {
+			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+			s1.moveMark();
+		}
+
+		// Left Up
+		if (y + 1 < 8 && x + 2 < 8) {
+			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+			s1.moveMark();
+		}
+
+		// Left Down
+		if (y - 2 >= 0 && x - 1 <= 0) {
+			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+			s1.moveMark();
+		}
+		
+		//Finish rest of these
 
 	}
 
