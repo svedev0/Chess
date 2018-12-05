b9b8b7bda1a01e215ecdc4c0a0c3c397f1fe5629
MasterEman <emanuel.kulich@gmail.com>
2018-12-05 12:43:10 +0100

    update

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -4,7 +4,6 @@ import board.ChessBoard;
 import board.Square;
 import javafx.scene.paint.Color;
 
-@SuppressWarnings("unused")
 public class Bishop extends Piece {
 
 	public Bishop(Color c) {
@@ -12,13 +11,13 @@ public class Bishop extends Piece {
 	}
 
 	public void showMove(int x, int y) {
-/*
+		
 		// Up Left
 		for (int i = 1; i < 8; i++) {
-			if (x - i < 0) {
+			if (x - i < 0 || y - i < 0) {
 				break;
 			}
-			Square s1 = ChessBoard.getSquare(x + i, y - i);
+			Square s1 = ChessBoard.getSquare(x - i, y - i);
 			if (!s1.hasPiece()) {
 				s1.mark(getColor());
 			} else {
@@ -26,50 +25,49 @@ public class Bishop extends Piece {
 				break;
 			}
 		}
-
+		
 		// Up Right
-		for (int i = 1; i < 8; i++) {
-			if (x + i > 7) {
-				break;
+			for (int i = 1; i < 8; i++) {
+				if (x + i > 7 || y - i < 0) {
+					break;
+				}
+				Square s1 = ChessBoard.getSquare(x + i, y - i);
+				if (!s1.hasPiece()) {
+					s1.mark(getColor());
+				} else {
+					s1.mark(getColor());
+					break;
+				}
 			}
-			Square s1 = ChessBoard.getSquare(x + i, y - i);
-			if (!s1.hasPiece()) {
-				s1.mark(getColor());
-			} else {
-				s1.mark(getColor());
-				break;
-			}
-		}
-
+				
 		// Down Left
-		for (int i = 1; i < 8; i++) {
-			if (y - i < 0) {
-				break;
+			for (int i = 1; i < 8; i++) {
+				if (x - i < 0 || y - i < 0) {
+					break;
+				}
+				Square s1 = ChessBoard.getSquare(x + i, y + i);
+				if (!s1.hasPiece()) {
+					s1.mark(getColor());
+				} else {
+					s1.mark(getColor());
+					break;
+				}
 			}
-			Square s1 = ChessBoard.getSquare(x + i, y - i);
-			if (!s1.hasPiece()) {
-				s1.mark(getColor());
-			} else {
-				s1.mark(getColor());
-				break;
+			
+		// Down Right
+			for (int i = 1; i < 8; i++) {
+				if (x + i > 7 || y - i < 0) {
+					break;
+				}
+				Square s1 = ChessBoard.getSquare(x - i, y + i);
+				if (!s1.hasPiece()) {
+					s1.mark(getColor());
+				} else {
+					s1.mark(getColor());
+					break;
+				}
 			}
 		}
-
-		// Down Right
-		for (int i = 1; i < 8; i++) {
-			if (y + i > 7) {
-				break;
-			}
-			Square s1 = ChessBoard.getSquare(x + i, y - i);
-			if (!s1.hasPiece()) {
-				s1.mark(getColor());
-			} else {
-				s1.mark(getColor());
-				break;
-			}
-		} */
-	}
-
 
 	public void hideMove() {
 
