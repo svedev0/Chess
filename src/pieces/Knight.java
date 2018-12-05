60d271f4e263ef09b122b06d6f67b1d27f9044cc
MasterEman <emanuel.kulich@gmail.com>
2018-12-05 21:39:22 +0100

    update

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -18,8 +18,16 @@ public class Knight extends Piece {
 			s1.moveMark();
 		}
 
-		// Up Left
-		if (y - 2 >= 0 && x - 1 >= 0) {
+		// Right Up
+
+		// Right Down
+
+		// Down Right
+
+		// Down Left
+
+		// Left Down
+		if (y - 2 >= 0 && x - 1 <= 0) {
 			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
 			s1.moveMark();
 		}
@@ -30,14 +38,11 @@ public class Knight extends Piece {
 			s1.moveMark();
 		}
 
-		// Left Down
-		if (y - 2 >= 0 && x - 1 <= 0) {
+		// Up Left
+		if (y - 2 >= 0 && x - 1 >= 0) {
 			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
 			s1.moveMark();
 		}
-		
-		//Finish rest of these
-
 	}
 
 	public void hideMove() {
