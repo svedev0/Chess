fae4ce5336f2652da86757124f4ee77fc22597b7
MasterEman <emanuel.kulich@gmail.com>
2018-12-05 21:53:45 +0100

    update

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -13,34 +13,50 @@ public class Knight extends Piece {
 	public void showMove(int x, int y) {
 
 		// Up Right
-		if (y - 2 >= 0 && x + 1 < 8) {
-			Square s1 = ChessBoard.getSquare(x + 1, y - 2);
+		if (x + 1 < 8 && y + 2 < 8) {
+			Square s1 = ChessBoard.getSquare(x + 1, y + 2);
 			s1.moveMark();
 		}
 
 		// Right Up
+		if (x + 2 < 8 && y + 1 < 8) {
+			Square s1 = ChessBoard.getSquare(x + 2, y + 1);
+			s1.moveMark();
+		}
 
 		// Right Down
+		if (x + 2 < 8 && y - 1 >= 0) {
+			Square s1 = ChessBoard.getSquare(x + 2, y - 1);
+			s1.moveMark();
+		}
 
 		// Down Right
+		if (x + 1 < 8 && y - 2 >= 0) {
+			Square s1 = ChessBoard.getSquare(x + 1, y - 2);
+			s1.moveMark();
+		}
 
 		// Down Left
+		if (x - 1 >= 0 && y - 2 >= 0) {
+			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+			s1.moveMark();
+		}
 
 		// Left Down
-		if (y - 2 >= 0 && x - 1 <= 0) {
-			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+		if (x - 2 >= 0 && y - 1 >= 0) {
+			Square s1 = ChessBoard.getSquare(x - 2, y - 1);
 			s1.moveMark();
 		}
 
 		// Left Up
-		if (y + 1 < 8 && x + 2 < 8) {
-			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+		if (x - 2 >= 0 && y + 1 < 8) {
+			Square s1 = ChessBoard.getSquare(x - 2, y + 1);
 			s1.moveMark();
 		}
 
 		// Up Left
-		if (y - 2 >= 0 && x - 1 >= 0) {
-			Square s1 = ChessBoard.getSquare(x - 1, y - 2);
+		if (x - 1 >= 0 && y + 2 < 8) {
+			Square s1 = ChessBoard.getSquare(x - 1, y + 2);
 			s1.moveMark();
 		}
 	}
