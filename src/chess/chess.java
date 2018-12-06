b998621944b34dfd31612031d871843e2319ac82
MasterEman <emanuel.kulich@gmail.com>
2018-12-06 09:10:40 +0100

    update

--- src/chess/chess.java
+++ src/chess/chess.java
@@ -6,17 +6,10 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import menu.StartMenu;
 
-@SuppressWarnings("unused")
 public class chess extends Application {
 
 	public void start(Stage primaryStage) throws Exception {
-
-		/*
-		StartMenu menu = new StartMenu();
-		Scene menuOverlay = new Scene(menu, Square.SIZE * 8, Square.SIZE * 8);
-		*/
 
 		ChessBoard board = new ChessBoard();
 		Scene scene = new Scene(board, Square.SIZE * 8 - 10, Square.SIZE * 8 - 10);
@@ -24,7 +17,6 @@ public class chess extends Application {
 		primaryStage.getIcons().add(new Image("file:/images/WHITE_KING.png"));
 		primaryStage.setTitle("Chess Game");
 		primaryStage.setResizable(false);
-	//primaryStage.setScene(menuOverlay);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
